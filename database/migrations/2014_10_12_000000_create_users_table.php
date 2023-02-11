ed36a312d3080ff4bc80e62b0af0fc0587acfb2a
galyym <ksgalym@mail.ru>
2023-02-12 01:10:00 +0500

    debug

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,18 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('lastname')->nullable();;
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('patronymic')->nullable();
+            $table->string('iin', 12)->nullable()->unique();
+            $table->string('email')->nullable();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone_number')->unique();
+            $table->string('profile_photo')->nullable();
+            $table->string('address')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->rememberToken()->nullable();
+            $table->string('firebase_token')->nullable();
             $table->timestamps();
         });
     }
